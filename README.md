c8f86ab9aafe81863943f54aaba51c984f47e779
Oluwaseun Agboola <ogeariyo@gmail.com>
2022-10-19 13:19:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Array Exercises
+
+Group Name **[group_five]**
+
+1. [Tahsin Khan]
+2. [Lubna Farheen Salauddinkhan]
+3. [Agboola Oluwaseun Esther]
+4. [Valentin Soare]
+5. [student name]
---
+++ src/main/java/se/lexicon/Exercise05.java
@@ -0,0 +1,25 @@
+package se.lexicon;
+
+public class Exercise05 {
+    public static void main(String[] args) {
+        ex5();
+    }
+
+    /**
+     * 5. Create a two-dimensional string array [2][2]. Assign values to the 2d array containing any Country and City.
+     * Expected output:
+     * France Paris
+     * Sweden Stockholm
+     */
+    public static void ex5() {
+        String[][] countries = {{"France", "Paris", "Norway"}, {"Sweden", "Stockholm", "Uppsala"}};
+        for (int i = 0; i < countries.length; i++) {
+            for (int j = 0; j < countries[i].length; j++){
+                System.out.print(countries[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+
+}
