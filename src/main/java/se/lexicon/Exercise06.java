6e2a4c83024faacd6dd6012fac6d4de9b1b82dac
Oluwaseun Agboola <ogeariyo@gmail.com>
2022-10-21 12:54:15 +0200

    completion of the remaining questions

--- src/main/java/se/lexicon/Exercise06.java
+++ src/main/java/se/lexicon/Exercise06.java
@@ -1,13 +1,23 @@
 package se.lexicon;
 
 public class Exercise06 {
+  public static void main(String[] args) {
+    ex6();
+  }
 
   /**
    * 6. Write a program which will set up an array to hold the next values in this order: 43, 5, 23, 17, 2, 14 and print the average of these 6 numbers.
    * Expected output: Average is: 17.3
    */
   public static void ex6() {
-
+    int[] numbers = {43, 5, 23, 17, 2, 14};
+    float sum = 0;
+    float average = 0;
+    for (int number : numbers) {
+      sum += number;
+      average = sum / 6;
+    }
+    System.out.printf("%.1f", average);
   }
 
 }
